c58ef910c6adba1f2b01410d3aa5042a34847047
d8505312 <52985855+d8505312@users.noreply.github.com>
2019-07-18 17:37:03 +0800

    圖片會動updata7185.

---
+++ assets/js/mian3.js
@@ -0,0 +1,93 @@
+(function(){
+    // Back to Top - by CodyHouse.co
+	var backTop = document.getElementsByClassName('js-cd-top1')[0],
+		offset = 150, // browser window scroll (in pixels) after which the "back to top" link is shown
+		offsetOpacity = 1200, //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		//scrollDuration = 700,
+		scrolling = false;
+
+	if( backTop ) {
+		//update back to top visibility on scrolling
+		window.addEventListener("scroll", function(event) {
+			if( !scrolling ) {
+				scrolling = true;
+				(!window.requestAnimationFrame) ? setTimeout(checkBackToTop, 250) : window.requestAnimationFrame(checkBackToTop);
+			}
+		});
+
+		//smooth scroll to top
+		backTop.addEventListener('click', function(event) {
+			
+			
+		});
+	}
+
+	function checkBackToTop() {
+		var windowTop = window.scrollY || document.documentElement.scrollTop;
+		( windowTop > offset ) ? Util.addClass(backTop, 'cd-top1--is-visible') : Util.removeClass(backTop, 'cd-top1--is-visible cd-top1--fade-out');
+		( windowTop > offsetOpacity ) && Util.addClass(backTop, 'cd-top1--fade-out');
+		scrolling = false;
+	}
+})();
+(function(){
+    // Back to Top - by CodyHouse.co
+	var backTop = document.getElementsByClassName('js-cd-top2')[0],
+		offset = 150, // browser window scroll (in pixels) after which the "back to top" link is shown
+		offsetOpacity = 1200, //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		//scrollDuration = 700,
+		scrolling = false;
+
+	if( backTop ) {
+		//update back to top visibility on scrolling
+		window.addEventListener("scroll", function(event) {
+			if( !scrolling ) {
+				scrolling = true;
+				(!window.requestAnimationFrame) ? setTimeout(checkBackToTop, 250) : window.requestAnimationFrame(checkBackToTop);
+			}
+		});
+
+		//smooth scroll to top
+		backTop.addEventListener('click', function(event) {
+			
+			(!window.requestAnimationFrame) ? window.scrollTo(0, 0) : Util.scrollTo(0, scrollDuration);
+		});
+	}
+
+	function checkBackToTop() {
+		var windowTop = window.scrollY || document.documentElement.scrollTop;
+		( windowTop > offset ) ? Util.addClass(backTop, 'cd-top2--is-visible') : Util.removeClass(backTop, 'cd-top2--is-visible cd-top2--fade-out');
+		( windowTop > offsetOpacity ) && Util.addClass(backTop, 'cd-top2--fade-out');
+		scrolling = false;
+	}
+})();
+(function(){
+    // Back to Top - by CodyHouse.co
+	var backTop = document.getElementsByClassName('js-cd-top3')[0],
+		offset = 150, // browser window scroll (in pixels) after which the "back to top" link is shown
+		offsetOpacity = 1200, //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		//scrollDuration = 700,
+		scrolling = false;
+
+	if( backTop ) {
+		//update back to top visibility on scrolling
+		window.addEventListener("scroll", function(event) {
+			if( !scrolling ) {
+				scrolling = true;
+				(!window.requestAnimationFrame) ? setTimeout(checkBackToTop, 250) : window.requestAnimationFrame(checkBackToTop);
+			}
+		});
+
+		//smooth scroll to top
+		backTop.addEventListener('click', function(event) {
+		
+			(!window.requestAnimationFrame) ? window.scrollTo(0, 0) : Util.scrollTo(0, scrollDuration);
+		});
+	}
+
+	function checkBackToTop() {
+		var windowTop = window.scrollY || document.documentElement.scrollTop;
+		( windowTop > offset ) ? Util.addClass(backTop, 'cd-top3--is-visible') : Util.removeClass(backTop, 'cd-top3--is-visible cd-top3--fade-out');
+		( windowTop > offsetOpacity ) && Util.addClass(backTop, 'cd-top3--fade-out');
+		scrolling = false;
+	}
+})();
